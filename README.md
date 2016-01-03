889240feae795084e610144d39546823e036975c
Silvana Trindade <syletri@gmail.com>
2016-01-03 16:29:48 -0200

    Atualizando help e build

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Developed by Silvana Trindade under mentorship by Claunir Pavan, during a research project called *Desenvolvimento de uma ferramenta computacional para a geração de topologias de redes de telecomunicações*, with financial support by PIBITI/CNPq at Universidade Federal da Fronteira Sul - UFFS.
+Developed by Silvana Trindade under mentorship by Claunir Pavan, during a research project called *Desenvolvimento de uma ferramenta computacional para a geração de topologias de redes de telecomunicações*, with financial support by PIBITI/CNPq and 281/UFFS/2015 at Universidade Federal da Fronteira Sul - UFFS.
+
+[Visit the project page for more information and help](http://ontgen.github.io/ontgen)
--- build.sh
+++ build.sh
@@ -5,24 +5,24 @@ then
 	mkdir "build"
 fi
 
-rm -rf ./packages/nttgen/data/nttgen
+rm -rf ./packages/ontgen/data/ontgen
 
-if [ ! -d "packages/nttgen/data/nttgen" ];
+if [ ! -d "packages/ontgen/data/ontgen" ];
 then
-	mkdir "packages/nttgen/data/nttgen"
-	mkdir "packages/nttgen/data/nttgen/platforms"
-	mkdir "packages/nttgen/data/nttgen/libs"
+	mkdir "packages/ontgen/data/ontgen"
+	mkdir "packages/ontgen/data/ontgen/platforms"
+	mkdir "packages/ontgen/data/ontgen/libs"
 fi
 
 ~/Qt/5.4/gcc/bin/qmake -makefile -o build/Makefile
 cd build
 make
-cp interface ../packages/nttgen/data/nttgen
-cp -r ./help ../packages/nttgen/data/nttgen
+cp interface ../packages/ontgen/data/ontgen
+cp -r ./help ../packages/ontgen/data/ontgen
 cd ..
 
-cp libqxcb.so packages/nttgen/data/nttgen/platforms
-cd packages/nttgen/data/nttgen
+cp libqxcb.so packages/ontgen/data/ontgen/platforms
+cd packages/ontgen/data/ontgen
 for dep in `ldd ./interface | awk '{print $3}' | grep -v "("`
 do
 	cp $dep ./libs
@@ -32,4 +32,4 @@ rm libs/libc.so.6
 
 cd ../../../../
 
-~/Qt/QtIFW2.0.1/bin/binarycreator -c config/config.xml -p packages/ --offline-only nttgen
+~/Qt/QtIFW2.0.1/bin/binarycreator -c config/config.xml -p packages/ --offline-only ontgen
